a37ac6c8c6b7071176893be22b7ddffb76fa907a
Saurav K <sauravkoduvalli@gmail.com>
2025-04-27 14:03:35 +0530

    code added for countdown

--- countdown_timer/script.js
+++ countdown_timer/script.js
@@ -1,12 +1,31 @@
-let countdownDateFrom = "1 Jan 2026";
+let countDownDate = "Jan 1, 2026 00:00:00";
 
-function countdown(date) {
-    const exprectedDate = new Date(date);
-    const currentDate = new Date();
+// this method will execute every each second.
+var x = setInterval(() => {
+  // use function to claculate the countdown
+  getCountDown(countDownDate);
+}, 1000);
 
-    let differenceInDates = exprectedDate - currentDate;
-    console.log(Date(differenceInDates).toString());
+function getCountDown(date) {
+  const exprectedDateTime = new Date(date).getTime();
+  const currentDateTime = new Date().getTime();
+
+  let diffInDates = exprectedDateTime - currentDateTime;
+
+  // time calculations for years, months, days, hours, minutes and seconds.
+  let days = Math.floor(diffInDates / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diffInDates % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)  );
+  let minutes = Mat.floor((diffInDates % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diffInDates % (1000 * 60)) / 1000);
+
+  // display the result in the elements with the help of id.
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (diffInDates < 0) {
+    clearInterval(x);
+    document.getElementsByClassName("big-text").innerHTML = 0;
+  }
 }
-
-
-countdown(countdownDateFrom);
